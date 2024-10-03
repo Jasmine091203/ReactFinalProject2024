7c01ae8796623b6527414e60423ea0658c33e68a
Wong, Tien-Yu <149470955+Jasmine091203@users.noreply.github.com>
2024-10-04 01:51:14 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RouterHome, { CombinedResultsLoader } from './components/RouterHome';
+import RootLayout from './components/RootLayout';
+import ErrorPage from './components/ErrorPage';
+
+import UniversityDetail, { UniversityDetailLoader } from './components/UniversityDetail';
+import UniversityPage, { UniversityLoader } from './components/UniversityPage';
+
+import HighSchoolsDetail, { HighSchoolsDetailLoader } from './components/HighSchoolsDetail';
+import HighSchoolsPage , { HighSchoolsLoader }from './components/HighSchoolsPage';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            { path: '/', element: <RouterHome />, loader: CombinedResultsLoader },
+            { path: '/university', element: <UniversityPage />, loader: UniversityLoader },
+            { path: '/university/:objectid', element: <UniversityDetail />, loader: UniversityDetailLoader },
+            { path: '/highSchool', element: <HighSchoolsPage /> ,loader: HighSchoolsLoader},
+            { path: '/highSchool/:objectid', element: <HighSchoolsDetail />, loader: HighSchoolsDetailLoader},
+        ]
+    }
+]);
+
+function App() {
+    return (
+        <div className="App">
+            <RouterProvider router={router} />
+        </div>
+    );
+}
+
+export default App;
